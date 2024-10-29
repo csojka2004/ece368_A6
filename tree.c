93015e623ec04c647ff22aec71ddf32f5fd1b86e
Christopher John Sojka <csojka@eceprog3.ecn.purdue.edu>
2024-10-28 20:19:13 -0400

    Done

--- tree.c
+++ tree.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 
 typedef struct TreeNode {
-    char type;
+    int label;
+    int letter;
     int width;
     int height;
     struct TreeNode* left;
@@ -14,15 +15,17 @@ TreeNode* buildTree(const char* filename) {
     FILE* file = fopen(filename, "r");
     if(!file) return NULL;
 
-    TreeNode* stack[100];
+    TreeNode* stack[10000];
     int top = -1;
     
     char line[100];
     while(fgets(line, sizeof(line), file)) {
         if(line[0] == 'V' || line[0] == 'H'){
             TreeNode* node = malloc(sizeof(TreeNode));
-            node->type = line[0];
-            node->left = node->right = NULL;
+            node->label = line[0];
+            node->letter = 1;
+            node->left = NULL;
+            node->right = NULL;
                  
             if(top >= 1){
                 node->right = stack[top--];
@@ -31,8 +34,10 @@ TreeNode* buildTree(const char* filename) {
             stack[++top] = node;
         }else{ // If we read 3(1,4) for example...
             TreeNode* node = malloc(sizeof(TreeNode));
-            sscanf(line, "%c(%d,%d)", &node->type, &node->width, &node->height);
-            node->left = node->right = NULL;
+            sscanf(line, "%d(%d,%d)", &node->label, &node->width, &node->height);
+            node->letter = 0;
+            node->left = NULL;
+            node->right = NULL;
             stack[++top] = node;
         }
     }
@@ -54,11 +59,11 @@ void freeTree(TreeNode* node) {
 
 void preorder(TreeNode* node, FILE* file) {
     if(node){
-        // Write the type of the node to the file
-        if(node->type == 'H' || node->type == 'V'){
-            fprintf(file, "%c\n", node->type);
+        // Write the label of the node to the file
+        if(node->letter){
+            fprintf(file, "%c\n", node->label);
         }else{
-            fprintf(file, "%c(%d,%d)\n", node->type, node->width, node->height);
+            fprintf(file, "%d(%d,%d)\n", node->label, node->width, node->height);
         }
 
         // Traverse the left subtree
@@ -79,29 +84,30 @@ void dimension(TreeNode* node, FILE* file){
         dimension(node->right, file);
 
         // Add on dimensions
-        if(node->type == 'H'){
+        if(node->letter && node->label == 'H'){
             node->width = node->left->width > node->right->width ? node->left->width : node->right->width;
             node->height = node->left->height + node->right->height;
-        }else if(node->type == 'V'){
+            fprintf(file, "%c(%d,%d)\n", node->label, node->width, node->height);
+        }else if(node->letter && node->label == 'V'){
             node->height = node->left->height > node->right->height ? node->left->height : node->right->height;
             node->width = node->left->width + node->right->width;
+            fprintf(file, "%c(%d,%d)\n", node->label, node->width, node->height);
+        }else{
+            fprintf(file, "%d(%d,%d)\n", node->label, node->width, node->height);
         }
-
-        fprintf(file, "%c(%d,%d)\n", node->type, node->width, node->height);
     }
 }
 
 void coords(TreeNode* node, FILE* file, int x, int y) {
     if(node){
         // If leaf node, write the coordinates
-        if(node->type != 'H' && node->type != 'V'){
-            fprintf(file, "%c((%d,%d)(%d,%d))\n", node->type, node->width, node->height, x, y);
-        
+        if(!node->letter){
+            fprintf(file, "%d((%d,%d)(%d,%d))\n", node->label, node->width, node->height, x, y);        
         // Otherwise, adjust the coordinates
-        }else if (node->type == 'H'){
+        }else if(node->letter && node->label == 'H'){
             coords(node->left, file, x, y + node->right->height);
             coords(node->right, file, x, y);
-        }else if (node->type == 'V'){
+        }else if(node->letter && node->label == 'V'){
             coords(node->left, file, x, y);
             coords(node->right, file, x + node->left->width, y);
         }
